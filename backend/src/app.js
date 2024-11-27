2131cc1079b513cb4b3216fb1970c8dea37a8891
sid446 <162982475+sid446@users.noreply.github.com>
2024-11-27 19:20:47 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+
+dotenv.config();
+const app=express();
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+
+})
+
+
+export {app}
+
